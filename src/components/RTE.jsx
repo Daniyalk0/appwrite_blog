3762c54a35926311763c86c3bb5cf56b0a2156b0
daniyalk0 <getdaniyalkhan@gmail.com>
2024-05-26 12:25:10 +0530

    added domain in tinymce

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,37 +1,60 @@
-import React, { useState } from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller} from 'react-hook-form';
+import React, { useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
 
-
-export default function RTE({name,control, label, defaultValue ="", ...props}) {
-
+export default function RTE({
+  name,
+  control,
+  label,
+  defaultValue = "",
+  ...props
+}) {
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <>
-        {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+        {label && <label className="inline-block mb-1 pl-1">{label}</label>}
         <Controller
           name={name || "content"}
           control={control}
           render={({ field: { onChange } }) => (
             <Editor
-              apiKey='ub34k04vlgyd2e480norjhzp9gzbwhciqrb1ikm1vvzcmxiq'
+              apiKey="ub34k04vlgyd2e480norjhzp9gzbwhciqrb1ikm1vvzcmxiq"
               initialValue={defaultValue}
               init={{
+                content_security_policy:
+                  "default-src 'self' https://appwrite-blog-nine-omega.vercel.app/",
                 initialValue: defaultValue,
                 height: 400,
                 menubar: true,
                 plugins: [
-                  "image", "advlist", "autolink", "lists", "link",
-                  "image", "charmap", "preview", "anchor", "searchreplace",
-                  "visualblocks", "code", "fullscreen", "insertdatetime",
-                  "media", "table", "code", "help", "wordcount", "anchor"
+                  "image",
+                  "advlist",
+                  "autolink",
+                  "lists",
+                  "link",
+                  "image",
+                  "charmap",
+                  "preview",
+                  "anchor",
+                  "searchreplace",
+                  "visualblocks",
+                  "code",
+                  "fullscreen",
+                  "insertdatetime",
+                  "media",
+                  "table",
+                  "code",
+                  "help",
+                  "wordcount",
+                  "anchor",
                 ],
                 toolbar:
                   "undo redo | blocks | image | bold italic forecolor | " +
                   "alignleft aligncenter bold italic forecolor | " +
                   "alignleft aligncenter alignright alignjustify | " +
                   "bullist numlist outdent indent | removeformat | help",
-                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                content_style:
+                  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
               }}
               {...props}
               onEditorChange={onChange}
@@ -39,6 +62,6 @@ export default function RTE({name,control, label, defaultValue ="", ...props}) {
           )}
         />
       </>
-  </div>
-  )
+    </div>
+  );
 }
